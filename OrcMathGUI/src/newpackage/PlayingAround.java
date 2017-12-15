9cae9a5810b75f63707baaa87ec328cd7dd3c681
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-12-15 10:49:23 -0500

    animation

--- OrcMathGUI/src/newpackage/PlayingAround.java
+++ OrcMathGUI/src/newpackage/PlayingAround.java
@@ -3,20 +3,27 @@ package newpackage;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+import guiTeacher.components.AnimatedComponent;
 import guiTeacher.components.Component;
 
-public class PlayingAround extends Component {
+public class PlayingAround extends AnimatedComponent {
 
 	public PlayingAround() {
-		super(40, 40, 100, 100);
+		super(40, 40, 180, 230 );
+		addSequence("resources/myspritesheet.png", 160, 0, 0, 180, 230, 5);
+		Thread animation = new Thread(this);
+		animation.start();
+		update();
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void update(Graphics2D g) {
-		g.setColor(Color.CYAN);
-
-		g.drawRect(0, 0, getWidth(), getHeight());
+		
+		super.update(g);
+		
+//		g.setColor(Color.CYAN);
+//		g.fillRect(0, 0, getWidth(), getHeight());
 	}
 	
 
